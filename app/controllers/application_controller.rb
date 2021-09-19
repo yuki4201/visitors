147005a224faefda1ac334ad2947ea5560c175b8
EC2 Default User <ec2-user@ip-172-31-3-177.ec2.internal>
2021-09-19 15:58:51 +0000

    add commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery :except => [:destroy]
     
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, only: [:show]
   
   protected
   
