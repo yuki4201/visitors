121f739a73d787417e4e63a3312a58c038196ea9
EC2 Default User <ec2-user@ip-172-31-3-177.ec2.internal>
2021-09-19 11:12:15 +0000

    migrateファイルを変更

--- db/migrate/20210914102810_add_is_valid_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIsValidToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :is_valid, :boolean, default: true, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_102810) do
+ActiveRecord::Schema.define(version: 2021_09_09_135507) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
