df02cf2d1fba319aad6b3e1657524c3bcea1d4b8
EC2 Default User <ec2-user@ip-172-31-3-177.ec2.internal>
2021-09-19 12:13:11 +0000

    Refile.secret_keyを追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = '4de4122c1f156da40e6e09222c72544189da303f185417f97ad178196334538c0a1ebd13e99f1a2ec11f29029cd3fda67c9902cf9348322dfb46bd3695d94434'
