f3a16a24dbdddfa2f0a51c60345a5e5f446e5b9e
EC2 Default User <ec2-user@ip-172-31-3-177.ec2.internal>
2021-10-19 03:04:35 +0000

    first_or_createを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
-class User < ApplicationRecord
+ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
-         
-  validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
   
   has_many :posts, dependent: :destroy
   has_many :plans, dependent: :destroy
@@ -18,7 +15,7 @@ class User < ApplicationRecord
 
   # omniauthのコールバック時に呼ばれるメソッド
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
